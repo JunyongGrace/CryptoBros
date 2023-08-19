a2f725b1fb1d9ac58005fe5970a72a11acb8fcdd
PrachponleuUch <ponleuvuthy@gmail.com>
2023-08-19 19:01:53 +1000

    Add AppNav and Logo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import ResponsiveAppBar from './components/AppNav';
+// import Logo from './images/logo.jpeg';
 // SUPPPPPPPPPPPPPPPPPP BOIIIIIIIIIIIIIIIIIISSSSSSSSSSSSSSSSS
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome to CryptoBros
-        </a>
-      </header>
+        <ResponsiveAppBar/>
     </div>
   );
 }
