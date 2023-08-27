e7f8e6688da93e03a538c531fc5e8e413dfb60f5
mengtong srean <mengtongsrean17@gmail.com>
2023-08-27 18:42:22 +1000

    Added Portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,9 +1,27 @@
-import * as React from 'react'; 
-function Portfolio(){
+import * as React from 'react';
+import PortfolioData from './PortfolioData';
+import './Portfolio.css'
+function Portfolio() {
+
+  const items = PortfolioData.map(item => {
+    return <div className="card">
+      <img className="card--photo" src={item.img} />
+      <p className="card--title">{item.title}</p>
+      <p><span className="bold">Price: ${item.price}</span></p>
+    </div>
+  })
+
   return (
     <>
-      <p>Portfolio</p>
+      <h1>'Profile Section'</h1>
+      <hr></hr>
+      <section className="cards-list">
+        {items}
+      </section>
+      <hr></hr>
+      <h1>'Transaction Section'</h1>
     </>
+
   )
 }
 export default Portfolio;
---
+++ src/pages/PortfolioData.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        img: "../nft-images/nft-img1.jpeg",
+        title: "Dee Dee",
+        price: "0.2ETH",
+    },
+    {
+        img: "../nft-images/nft-img2.jpeg",
+        title: "Cool Monkey",
+        price: "0.9ETH",
+    },
+    {
+        img: "../nft-images/nft-img3.jpeg",
+        title: "Dee Dee",
+        price: '0.2ETH',
+    },
+    {
+        img: "../nft-images/nft-img4.jpeg",
+        title: "Cool Monkey",
+        price: "0.9ETH",
+    },
+    {
+        img: "../nft-images/nft-img5.jpeg",
+        title: "Dee Dee",
+        price: '0.2ETH',
+    },
+    {
+        img: "../nft-images/nft-img6.jpeg",
+        title: "Cool Monkey",
+        price: "0.9ETH",
+    }
+]
