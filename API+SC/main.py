3acf291e3fa001db9989cbe038ce035f106822ab
PrachponleuUch <ponleuvuthy@gmail.com>
2023-10-14 19:01:33 +1100

    Bug Fix: Contract now only creates once

--- API+SC/main.py
+++ API+SC/main.py
@@ -23,13 +23,6 @@ app.add_middleware(
 
 # type your address here
 w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
-# Default is 1337 or with the PORT in your Gaanche
-chain_id = 1337
-# Find in you account (Get this from Ganache)
-my_address = w3.eth.accounts[0]
-# Find in you account (Get this from Ganache)
-private_key = "0x18d0049a270b9c841d0c5a062a6803144f1d9f003e13b4699970d17082422ebe"
-
 # Read Smart Contract
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
@@ -59,32 +52,44 @@ bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"
 # get abi
 abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 
-SimpleStorage = w3.eth.contract(address=my_address, abi=abi, bytecode=bytecode)
-
-nonce = w3.eth.get_transaction_count(my_address)
-
-transaction = SimpleStorage.constructor().build_transaction(
-    {
-        "chainId": chain_id,
-        "gasPrice": w3.eth.gas_price,
-        "from": my_address,
-        "nonce": nonce,
-    }
-)
-transaction.pop('to')
-
-signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
-# print(transaction)
-tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
-tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+tx_receipt = None
 
 @app.get("/transaction/get/")
 async def getTransaction(request=Request , sender: Optional[str] = None, pk: Optional[str] = None, receiver: Optional[str] = None, amount: Optional[float] = None, senderId: Optional[int] = None, nftId: Optional[int] = None):
+    global tx_receipt
+    if (not tx_receipt):    
+        # Default is 1337 or with the PORT in your Gaanche
+        chain_id = 1337
+        # Find in you account (Get this from Ganache)
+        my_address = w3.eth.accounts[5]
+        # Find in you account (Get this from Ganache)
+        private_key = "0x555b56ea3ab677c71c937f733a3411ca60f7cdbd23c968e1e094876c20aa3c33"
+
+        SimpleStorage = w3.eth.contract(address=my_address, abi=abi, bytecode=bytecode)
+
+        nonce = w3.eth.get_transaction_count(my_address)
+
+        transaction = SimpleStorage.constructor().build_transaction(
+            {
+                "chainId": chain_id,
+                "gasPrice": w3.eth.gas_price,
+                "from": my_address,
+                "nonce": nonce,
+            }
+        )
+        transaction.pop('to')
+
+        signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+        tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+        tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+    
     simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+    
+    print(type(simple_storage))
     
     # For transaction
     transaction = {
-        'from': my_address,
+        'from': w3.eth.accounts[5],
         "value": w3.to_wei(1, 'ether')
     }
     
@@ -111,7 +116,7 @@ async def getTransaction(request=Request , sender: Optional[str] = None, pk: Opt
                 return {"error": "Failed to connect to the database."}
         cursor = connection.cursor()
         sqlInsert = f"INSERT INTO Transaction (purchTime, senderAddr, receiverAddr, tranHash) VALUES (%s, %s, %s, %s)"
-        val = (time, my_address, receiveR, transaction_hash.hex())
+        val = (time, w3.eth.accounts[5], receiveR, transaction_hash.hex())
         cursor.execute(sqlInsert, val)
         connection.commit()
     except mysql.connector.Error as err:
@@ -128,8 +133,8 @@ async def getTransaction(request=Request , sender: Optional[str] = None, pk: Opt
     
     output = {
         "tx": transaction_hash.hex(),
-        "sender": my_address,
-        "balance": w3.eth.get_balance(my_address)*(10**(-18)),
+        "sender": w3.eth.accounts[5],
+        "balance": w3.eth.get_balance(w3.eth.accounts[5])*(10**(-18)),
         "receiver": receiveR,
         "amount": simple_storage.functions.getAllTransactions().call()[transactions_length - 1][2]*(10**(-18)),
         "time": time,
